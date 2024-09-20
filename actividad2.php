14969ae32607cf61c188817241e94ec71552f22d
Mxglzr <mangelsanchez2007@gmail.com>
2024-09-20 14:25:03 -0500

    Delete actividad2.php

--- actividad2.php
+++
@@ -1,9 +0,0 @@
-
-<form method="post" action="resultadoA2.php">
-    <p> <h1>CÁLCULO DEL VOLUMEN DE UN CILINDRO </h1></p>  <br>
-    introtuzca el diámetro en metros:
-    <input type="number" name="txtDiam" value=""> <br> <br>
-    introduzca la altura en metros:
-    <input type="number" name="txtAltu" value=""> <br> <br>
-    <input value="Calcular" type="submit">
-</form>
